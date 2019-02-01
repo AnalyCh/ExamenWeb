58befb2495718e61b54f8f7b34c304ddd7cecb09
AnalyCh <analy_chinacalle@hotmail.com>
2019-02-01 16:19:20 -0600

    auto conductor entity

--- examen-autos/src/app.module.ts
+++ examen-autos/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: "mysql",
+            host: "localhost",
+            port: 32769,
+            database: 'vehiculos',
+            username: 'root',
+            password: 'root',
+            synchronize: true,
+            dropSchema: true,
+            entities:[
+                //AutoEntity
+            ]
+        })
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
